542148635586e37ee7ced34ac98ecae213b8a29e
GrantWatts <gwatts@egi.utah.edu>
2015-01-30 10:59:38 -0700

    changed from hello world to google proxy for testing

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-http = require('http');
+var http = require('http'),
+    httpProxy = require('http-proxy');
 var port = process.env.port || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
+//http.createServer(function(req, res) {
+//  res.writeHead(200, { 'Content-Type': 'text/plain' });
+//  res.end('Hello World\n');
+//}).listen(port);
+httpProxy.createServer({
+    target: 'https://google.com',
+    headers: {
+        host: 'google.com'
+    }
 }).listen(port);
