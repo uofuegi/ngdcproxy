530f2589b8f7878970cd9fc8b9a5dbfa3353dedb
GrantWatts <gwatts@egi.utah.edu>
2015-01-30 11:02:24 -0700

    switching back with npm package still installed

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var http = require('http'),
     httpProxy = require('http-proxy');
 var port = process.env.port || 1337;
-//http.createServer(function(req, res) {
-//  res.writeHead(200, { 'Content-Type': 'text/plain' });
-//  res.end('Hello World\n');
-//}).listen(port);
-httpProxy.createServer({
-    target: 'https://google.com',
-    headers: {
-        host: 'google.com'
-    }
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
 }).listen(port);
+//httpProxy.createServer({
+//    target: 'https://google.com',
+//    headers: {
+//        host: 'google.com'
+//    }
+//}).listen(port);
